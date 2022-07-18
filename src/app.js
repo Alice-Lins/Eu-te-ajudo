aff3dcc13bdf66ebfe1414ff2d90449c66c3367b
Alice_Lins <alicelins17@yahoo.com>
2022-07-18 20:57:15 -0300

    instalaçoes das dependencias

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+require('dotenv-safe').config();
+
+const db = require('./database/mongoConfig');
+db.connect();
+
+//app.use(express.json());
+
+// const noteRoutes = require('./routes/noteRoutes');
+// const tagRoutes = require('./routes/tagRoutes');
+
+// //app.use("/notes", noteRoutes);
+// app.use("/tags", tagRoutes);
+
+module.exports = app;
