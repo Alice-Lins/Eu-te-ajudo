bdd0d8be4e6cbfc2ac124b6c3b5056639ef4527e
Alice_Lins <alicelins17@yahoo.com>
2022-07-23 13:57:04 -0300

    adicionando procfile

--- src/app.js
+++ src/app.js
@@ -11,10 +11,20 @@ db.connect();
 
 app.use(express.json());
 
+
  const teacherRoutes = require('./Routes/teacherRoutes');
 // const tagRoutes = require('./routes/tagRoutes');
 
 app.use("/teacher", teacherRoutes);
 // app.use("/tags", tagRoutes);
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        title: "Reprograma -> Projeto Final",
+        version: "1.0.0",
+        mensagem: "Olá mundooooo, bem vindo ao meu site!"
+    })
+})
+
 
 module.exports = app;
